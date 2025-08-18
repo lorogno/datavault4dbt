1119e831adcc496d96a17138b669c8a66c1bea47
tkiehn <162969167+tkiehn@users.noreply.github.com>
2025-08-18 11:21:47 +0200

    add execute check

--- macros/internal/metadata_processing/yaml_metadata_parser.sql
+++ macros/internal/metadata_processing/yaml_metadata_parser.sql
@@ -1,8 +1,5 @@
 {% macro yaml_metadata_parser(name=none, yaml_metadata=none, parameter=none, required=False, documentation=none) %}
-{% if not execute %}
-    {% do return('none') %}
-{% else %}
-
+{% if execute %}
     {% if datavault4dbt.is_something(yaml_metadata) %}
         {%- set metadata_dict = fromyaml(yaml_metadata) -%}
         {% if name in metadata_dict.keys() %}
